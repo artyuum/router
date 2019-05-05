7672bbe122e7f29da3e067dc2d6d8c4791e8734c
Artyum <artyum@protonmail.com>
2019-05-05 19:46:45 +0400

    inital commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# router
+Yet another PHP request router.
+
+## Contributing
+If you'd like to contribute, please fork the repository and make changes as you'd like. Pull requests are warmly welcome.
---
+++ src/Router/Router.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Artyum;
+
+/**
+ * Class Router
+ *
+ * This is the Router class that is used to route the incoming request to the appropriate controller.
+ */
+class Router
+{
+
+}
